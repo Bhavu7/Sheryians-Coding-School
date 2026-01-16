0264ec3c4249b783adb3fcc384c009cc534ce615
Bhavesh Bhoi <bhavubhoi806@gmail.com>
2026-01-16 16:38:42 +0530

    Added:Random Color Generator Mini Project Updated

--- JavaScript/Practice/Random Color Generator/script.js
+++ JavaScript/Practice/Random Color Generator/script.js
@@ -1,6 +1,16 @@
 var btn = document.querySelector("button");
-var displayColor = document.querySelector("#colorDisplay");
+var colorDisplay = document.querySelector("#colorDisplay");
+var colorCode = document.querySelector("#colorCode");
 
 btn.addEventListener("click", function () {
-  let randomColor = Math.floor(Math.random()*256);
+  let redColor = Math.floor(Math.random() * 256);
+  let greenColor = Math.floor(Math.random() * 256);
+  let blueColor = Math.floor(Math.random() * 256);
+
+  const color = `rgb(${redColor}, ${greenColor}, ${blueColor})`;
+
+  colorDisplay.style.backgroundColor = color;
+  colorCode.textContent = color;
+  //   colorCode.innerHTML = `rgb(${redColor}, ${greenColor}, ${blueColor})`;
+  //   colorCode.innerText = `rgb(${redColor}, ${greenColor}, ${blueColor})`;
 });
