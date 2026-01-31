055223947e51870b86274e29f1350ee9e6224cd2
Bhavesh Bhoi <bhavubhoi806@gmail.com>
2026-01-31 11:20:59 +0530

    Added:Scramble Text Effect Project

--- JavaScript/Practice/Scramble Text Effect/script.js
+++ JavaScript/Practice/Scramble Text Effect/script.js
@@ -1,5 +1,19 @@
-const p = document.querySelector('p');
+const h1 = document.querySelector("h1");
+const characters =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:,.<>?/";
+const text = h1.innerText;
 
-p.addEventListener('mouseenter',()=>{
-    
-})
+h1.addEventListener("mouseenter", () => {
+  setInterval(() => {
+    const str = text
+      .split("")
+      .map((char, index) => {
+        return characters.split("")[
+          Math.floor(Math.random() * characters.length)
+        ];
+      })
+      .join("");
+
+    h1.innerText = str;
+  }, 50);
+});
