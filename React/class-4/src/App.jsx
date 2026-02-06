3e3fc6f6e8656b8643d359fa3a132814a7826171
Bhavesh Bhoi <bhavubhoi806@gmail.com>
2026-02-06 19:32:37 +0530

    Added:Conditional Rendering in React

--- React/class-4/src/App.jsx
+++ React/class-4/src/App.jsx
@@ -1,10 +1,25 @@
+// import "./App.css";
+// import Navbar from "./Components/Navbar";
+
+// function App() {
+//   return (
+//     <>
+//       <Navbar />
+//     </>
+//   );
+// }
+
+// export default App;
+
 import "./App.css";
 import Navbar from "./Components/Navbar";
 
+
 function App() {
+  const isLoggedIn = false;
   return (
     <>
-      <Navbar />
+      {isLoggedIn == true ? <Navbar /> : alert("Please Login")}
     </>
   );
 }
