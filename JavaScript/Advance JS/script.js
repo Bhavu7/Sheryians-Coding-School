2bc78140a5fdc1c648cc3b1b5a024a3c0b60ed8d
Bhavesh Bhoi <bhavubhoi806@gmail.com>
2026-02-07 10:18:22 +0530

    Added:Understood Classes and Classes With Constructor

--- JavaScript/Advance JS/script.js
+++ JavaScript/Advance JS/script.js
@@ -7,3 +7,28 @@ let user = {
 };
 
 console.log(user);
+
+// Classes
+
+class SmartPhone {
+  constructor() {
+    this.Brand = "SamSung";
+    this.Series = "S";
+    this.Model = "S26";
+    this.Price = 135000;
+  }
+}
+
+let Phone1 = new SmartPhone()
+
+// Class with Constructor
+class SmartPhone1 {
+  constructor(brand, series, model, price) {
+    this.Brand = brand;
+    this.Series = series;
+    this.Model = model;
+    this.Price = price;
+  }
+}
+
+let Phone2 = new SmartPhone1("Samsung", "S", "S25", 115000)
