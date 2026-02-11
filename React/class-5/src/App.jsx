e118264fb2315e63f9edf4779371eb712432b25e
Bhavesh Bhoi <bhavubhoi806@gmail.com>
2026-02-11 11:17:04 +0530

    Added:Created Seperate Component for Student Marks

--- React/class-5/src/App.jsx
+++ React/class-5/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Counter from "./Components/Counter";
 import RandomNumberGenerator from "./Components/RandomNumberGenerator";
+import StudentMarks from "./Components/StudentMarks";
 
 function App() {
   return (
     <>
-      <RandomNumberGenerator />
+      {/* <RandomNumberGenerator /> */}
       {/* <Counter /> */}
+      <StudentMarks />
     </>
   );
 }
---
+++ React/class-5/src/Components/StudentMarks.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+const StudentMarks = () => {
+  const [marks, setMarks] = useState([45, 76, 36, 89, 29]);
+
+  function addGraceMarks() {
+    let newMarks = marks.map(function (mark) {
+      return mark + 5;
+    });
+
+    setMarks(newMarks);
+  }
+  return (
+    <div>
+      {marks.map(function (mark, index) {
+        return (
+          <h1 key={index}>
+            Marks of Subject {index} : {mark} ({mark >= 33 ? "PASS" : "FAIL"})
+          </h1>
+        );
+      })}
+      <button onClick={addGraceMarks}>Add Grace</button>
+    </div>
+  );
+};
+
+export default StudentMarks;
