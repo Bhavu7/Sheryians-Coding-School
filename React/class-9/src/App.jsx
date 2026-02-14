15b3ccce9eb137b5644543077647f55e2d13c1f6
Bhavesh Bhoi <bhavubhoi806@gmail.com>
2026-02-14 10:22:40 +0530

    Added:Designed UI

--- React/class-9/src/App.jsx
+++ React/class-9/src/App.jsx
@@ -1,21 +1,48 @@
+import { useState } from "react";
 import "./App.css";
-import axios from 'axios'
+import axios from "axios";
 
 function App() {
+  const [allData, setAllData] = useState([]);
   async function getData() {
-    const response = await axios.get('https://picsum.photos/v2/list?page=2&limit=100')
-    console.log(response.data[0]);
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/users",
+    );
+    setAllData(response.data);
 
     // Destructuring
-    const {data} = await axios.get('https://picsum.photos/v2/list?page=2&limit=100')
-    console.log(data);
+    // const { data } = await axios.get(
+    //   "https://jsonplaceholder.typicode.com/users",
+    // );
+    // console.log(data);
   }
 
   return (
-    <>
-      <h1>App Here!</h1>
-      <button onClick={getData}>Get Data</button>
-    </>
+    <div className="app">
+      <h1 className="title">Understanding Axios!</h1>
+      <button className="btn" onClick={getData}>
+        Get Data
+      </button>
+
+      <div className="card-container">
+        {allData.map((elem, index) => {
+          return (
+            <div className="card" key={index}>
+              <h2 className="card-id">User ID: {elem.id}</h2>
+              <p>
+                <span>Name:</span> {elem.name}
+              </p>
+              <p>
+                <span>Username:</span> {elem.username}
+              </p>
+              <p>
+                <span>Email:</span> {elem.email}
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 }
 
